d2a1dfd20e32c66157f7e48311c1ac6e10219d6d
GGuiTC <guitcard@gmail.com>
2024-10-18 19:23:05 -0300

    edita, mas da erro :angry:

--- cont_perfil/controlPerfil.js
+++ cont_perfil/controlPerfil.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const Perfil = require('./Perfil');
 
 const adminAut = require('../middleware/adminAutoriz');
+const { where } = require('sequelize');
 
 router.use(bodyParser.urlencoded({extended: true}));
 
@@ -11,7 +12,6 @@ router.get("/perfil_page",adminAut, (req,res)=>{
     let id_usuario = req.session.usuario.id;
     let nome = req.session.usuario.nome;
     let email = req.session.usuario.email;
-    //arruma
     Perfil.findOne({
         where: {id_usuario: id_usuario}
     }).then((perfil)=>{
@@ -31,6 +31,9 @@ router.get("/perfil_page",adminAut, (req,res)=>{
 })
 
 router.post("/cadastra-edita-perfil", (req,res)=>{
+    // TEM Q ARRUMAR:
+    // 1 - SE O DADO NO BANCO DE DADOS E NO FORM FOR IGUAL, NÃO EDITA
+    // 2 - TELA DE LOADING DEPOIS DE ATUALIZAR E DEPOIS REDIRECIONAR PARA /perfil_page
     let id_perfil = req.body.id;
     let nome = req.body.nome;
     let email = req.body.email;
@@ -43,19 +46,14 @@ router.post("/cadastra-edita-perfil", (req,res)=>{
 
     
     // --------------- NOME ---------------
+    // ARRUMAR PARA QUE, SE FOR ALTERADO, MUDE NA TEBELA USUARIO TAMBEM
     Perfil.findOne({
-        where: {id_perfil: id_perfil}
+        where: {id_usuario: id_perfil}
     }).then((perfil)=>{
-        if(perfil == undefined){
-            Perfil.create({
-                nome: nome
-            }).then((perfil)=>{
-                res.render("perfil/perfil-page", {perfil});
-            })
-        }else{
+        if(perfil != undefined){
             Perfil.update({
                 nome: nome
-            }).then((perfil)=>{
+            },{where: {id_usuario: id_perfil}}).then((perfil)=>{
                 res.render("perfil/perfil-page", {perfil});
             })
         }
@@ -64,11 +62,94 @@ router.post("/cadastra-edita-perfil", (req,res)=>{
 
 
     // --------------- EMAIL ---------------
+    // ARRUMAR PARA QUE, SE FOR ALTERADO, MUDE NA TEBELA USUARIO TAMBEM
+    Perfil.findOne({
+        where: {id_usuario: id_perfil}
+    }).then((perfil)=>{
+        if(perfil != undefined){
+            Perfil.update({
+                email: email
+            },{where: {id_usuario: id_perfil}}).then((perfil)=>{
+                res.render("perfil/perfil-page", {perfil});
+            })
+        }
+    })
+
+
+
     // --------------- TELEFONE ---------------
+    Perfil.findOne({
+        where: {id_usuario: id_perfil}
+    }).then((perfil)=>{
+        if(perfil != undefined){
+            Perfil.update({
+                telefone: telefone
+            },{where: {id_usuario: id_perfil}}).then((perfil)=>{
+                res.render("perfil/perfil-page", {perfil});
+            })
+        }
+    })
+
+
+
     // --------------- SEXO ---------------
+    Perfil.findOne({
+        where: {id_usuario: id_perfil}
+    }).then((perfil)=>{
+        if(perfil != undefined){
+            Perfil.update({
+                sexo: sexo
+            },{where: {id_usuario: id_perfil}}).then((perfil)=>{
+                res.render("perfil/perfil-page", {perfil});
+            })
+        }
+    })
+
+
+
     // --------------- CPF ---------------
+    Perfil.findOne({
+        where: {id_usuario: id_perfil}
+    }).then((perfil)=>{
+        if(perfil != undefined){
+            Perfil.update({
+                cpf: cpf
+            },{where: {id_usuario: id_perfil}}).then((perfil)=>{
+                res.render("perfil/perfil-page", {perfil});
+            })
+        }
+    })
+
+
+
     // --------------- RG ---------------
+    Perfil.findOne({
+        where: {id_usuario: id_perfil}
+    }).then((perfil)=>{
+        if(perfil != undefined){
+            Perfil.update({
+                rg: rg
+            },{where: {id_usuario: id_perfil}}).then((perfil)=>{
+                res.render("perfil/perfil-page", {perfil});
+            })
+        }
+    })
+
+
+
     // --------------- DATA_NASC ---------------
+    Perfil.findOne({
+        where: {id_usuario: id_perfil}
+    }).then((perfil)=>{
+        if(perfil != undefined){
+            Perfil.update({
+                data_nasc: data_nasc
+            },{where: {id_usuario: id_perfil}}).then((perfil)=>{
+                res.render("perfil/perfil-page", {perfil});
+            })
+        }
+    })
+
 
 
     // A Ideia é: 
