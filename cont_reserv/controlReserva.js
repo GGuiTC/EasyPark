ec7fa1ee1df1a15c6d72b068328859d5e795461e
GGuiTC <guitcard@gmail.com>
2024-10-31 11:07:25 -0300

    página de reservas funcionando totalmente

--- cont_reserv/controlReserva.js
+++ cont_reserv/controlReserva.js
@@ -2,15 +2,39 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const Reserva = require('./Reserva');
+const Perfil = require('../cont_perfil/Perfil')
 
 const adminAut = require('../middleware/adminAutoriz');
+const { where } = require('sequelize');
 
 router.use(bodyParser.urlencoded({extended: true}));
 
-router.get("/reserv_page",adminAut,(req,res)=>{
-    id = req.session.usuario.id;
-    res.render("reserv/reserv-page", { id });
-})
+
+router.get("/reserv_page", adminAut, (req, res) => {
+    const id = req.session.usuario.id;
+
+    Perfil.findOne({
+        where: { id_usuario: id }
+    }).then((perfil) => {
+        const id_perfil = perfil.id_perfil;
+
+        Reserva.findAll({
+            where: { id_perfil: id_perfil },
+            include: [
+                { model: Vaga, as: 'vaga', attributes: ['numero'] },
+                { model: Veiculo, as:'veiculo', attributes: ['marca', 'modelo', 'cor', 'placa'] }
+            ]
+        }).then((reservas) => {
+            res.render("reserv/reserv-page", { reservas, id });
+        }).catch((error) => {
+            console.error("Erro ao buscar reservas:", error);
+            res.status(500).send("Erro ao buscar reservas.");
+        });
+    }).catch((error) => {
+        console.error("Erro ao buscar perfil:", error);
+        res.status(500).send("Erro ao buscar perfil.");
+    });
+});
 
 router.get("/reserva_vaga/:id", (req, res)=>{
     id_vaga = req.params.id;
